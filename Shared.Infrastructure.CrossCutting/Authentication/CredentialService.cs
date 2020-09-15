0fa07531ecb311118c868f745d7d99be9d4633bb
uciel.rodriguez <uciel.rodriguez@endava.com>
2020-09-15 15:27:25 -0300

    master <- add Choby credential

--- Shared.Infrastructure.CrossCutting/Authentication/CredentialService.cs
+++ Shared.Infrastructure.CrossCutting/Authentication/CredentialService.cs
@@ -1,13 +1,34 @@
-﻿namespace Shared.Infrastructure.CrossCutting.Authentication
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Shared.Infrastructure.CrossCutting.Authentication
 {
     public class CredentialService : ICredentialService
     {
+        private readonly IList<Credential> _validCredentials = new List<Credential>
+        {
+            new Credential
+            {
+                Id = "InventApp",
+                SecretKey = "1nfr4structur3_1nv3nt4pp"
+            },            
+            new Credential
+            {
+                Id = "Insfrastructure",
+                SecretKey = "1nfr4structur3_1nfr4structur3"
+            },
+            new Credential
+            {
+                Id = "Choby",
+                SecretKey = "1nfr4structur3_ch0by"
+            }
+        };
+
         public bool AreValid(Credential credential)
         {
             if (credential == null) return false;
 
-            return credential.Id == "InventApp" && credential.SecretKey.Equals("1nfr4structur3_1nv3nt4pp") ||
-                   credential.Id == "Insfrastructure" && credential.SecretKey.Equals("1nfr4structur3_1nfr4structur3");
+            return _validCredentials.Any(vc => vc.Id == credential.Id && vc.SecretKey == credential.SecretKey);
         }
     }
 }
